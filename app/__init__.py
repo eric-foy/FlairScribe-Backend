b66b71bf604ceda9d407c7bc07ba43674e835209
Erica Foy <erica.foy@flairdocs.com>
2025-03-25 12:11:14 -0400

    Update cors.

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app, resources={"/speechbox": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
                             "/hello": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
-                            "/process-transcription": {"origins": ["http://localhost:5150", "https://localhost:7054"]}
+                            "/vernacular": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
+                            "/transcribe": {"origins": ["http://localhost:5150", "https://localhost:7054"]}
                             })
 app.config['CORS_HEADERS'] = 'Content-Type'
 
