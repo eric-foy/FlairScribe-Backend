05f8949a7e80cef780e6c0a4e287df742c8aacbc
Erica Foy <erica.foy@flairdocs.com>
2025-03-26 14:16:25 -0400

    Add backend cors allow all origin. Communication is done with localhost and not over internet so is only minor security.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app, resources={"/speechbox": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
-                            "/hello": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
-                            "/vernacular": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
-                            "/transcribe": {"origins": ["http://localhost:5150", "https://localhost:7054"]}
+# cors = CORS(app, resources={"/speechbox": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
+#                             "/hello": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
+#                             "/vernacular": {"origins": ["http://localhost:5150", "https://localhost:7054"]},
+#                             "/transcribe": {"origins": ["http://localhost:5150", "https://localhost:7054"]}
+#                             })
+cors = CORS(app, resources={"/speechbox": {"origins": "*"},
+                            "/hello": {"origins": "*"},
+                            "/vernacular": {"origins": "*"},
+                            "/transcribe": {"origins": "*"}
                             })
 app.config['CORS_HEADERS'] = 'Content-Type'
 
