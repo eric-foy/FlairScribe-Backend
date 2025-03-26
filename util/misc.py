1dce0d5991a5b86f0b1db94c4abe22bdc7da478d
Erica Foy <erica.foy@flairdocs.com>
2025-03-26 13:54:27 -0400

    Indent was wrong for delete_files_in_directory

--- util/misc.py
+++ util/misc.py
@@ -5,8 +5,8 @@ def delete_files_in_directory(directory_path):
         files = os.listdir(directory_path)
         for file in files:
             file_path = os.path.join(directory_path, file)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
         print("All files deleted successfully.")
     except OSError:
